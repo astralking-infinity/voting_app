ba91a115614af089b0ea96e547dc3cf020545c89
astralking <mao.chiu.astral@gmail.com>
2018-11-20 02:20:23 +0800

    Fixed url

--- config/urls.py
+++ config/urls.py
@@ -27,7 +27,7 @@ from rest_framework.routers import DefaultRouter
 # router.register('choices', views.ChoiceViewSet)
 
 urlpatterns = [
-    path('', RedirectView.as_view(url='polls/')),
+    path('', RedirectView.as_view(url='api/')),
     path('api/', include('polls_api.urls')),
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
